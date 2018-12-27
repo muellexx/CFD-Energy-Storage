cceab69c520ba8c0460e7b5892e17bf924c752f0
muellexx <46179416+muellexx@users.noreply.github.com>
2018-12-27 16:47:02 +0900

    Add files via upload

---
+++ include/energy.h
@@ -0,0 +1,16 @@
+#ifndef ENERGY_H_INCLUDED
+#define ENERGY_H_INCLUDED
+
+void
+ENERGY_check();
+
+void
+ENERGY_exergy();
+
+void
+ENERGY_efficiency();
+
+double
+ENERGY_capacity();
+
+#endif // ENERGY_H_INCLUDED
---
+++ include/mms.h
@@ -0,0 +1,13 @@
+#ifndef MMS_H_INCLUDED
+#define MMS_H_INCLUDED
+
+void
+MMS_Calculate(void);
+
+void
+MMS_FTBSSimplified(void);
+
+void
+MMS_FTBSCharging(void);
+
+#endif // MMS_H_INCLUDED
